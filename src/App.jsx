3bcd5a6ff253dfa6a8f7e9a13423953e71d0811d
Susheel <susheelrai96@gmail.com>
2024-04-11 13:59:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./assets/components/Header"
+import Footer from "./assets/components/Footer"
+import Note from "./assets/components/Note"
+
+
+function App() {
+
+  return (
+    <>
+    <div className="app">
+      <Header />
+      <Note />
+      <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Note.jsx
@@ -0,0 +1,13 @@
+const Note = ()=>{
+
+    return (
+        <>
+            <div className="note">
+                <h1>This is a note title</h1>
+                <p>This is a Note!</p>
+            </div>
+        </>
+    )
+}
+
+export default Note;
